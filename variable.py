99cbd7a33b834c8bbbdad9fffd901b3c85126612
Elaf24 <alhadi.elaf2428@gmail.com>
2024-12-30 09:57:22 +0600

    testing

---
+++ variable.py
@@ -0,0 +1,20 @@
+# Creating Variables
+# Python has no command for declaring a variable.
+#
+# A variable is created the moment you first assign a value to it.
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+
+# Casting
+# If you want to specify the data type of a variable, this can be done with casting.
+#
+# Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
